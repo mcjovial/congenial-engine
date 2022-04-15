5b5c4eb73e9819bff3e6f4760e1c04930a74cc25
mcjovial <emmanuelelias455@gmail.com>
2022-04-15 20:48:04 +0100

    static files links

--- src/configs/website/index.js
+++ src/configs/website/index.js
@@ -1,5 +1,6 @@
 // Use this for Local Development
+export const WEBSITE_URL = "http://biba.test";
 // export const WEBSITE_URL = "http://127.0.0.1/foodomaa";
 
 // Comment out the above and Uncomment the below line before building the files for Production
-export const WEBSITE_URL = "https://" + window.location.hostname;
+// export const WEBSITE_URL = "https://" + window.location.hostname;
